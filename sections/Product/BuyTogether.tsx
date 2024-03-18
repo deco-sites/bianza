cab2fd030a4c93270778e4bb77e88d80fbd51482
FERDZ  - Agência Digital <88567084+ferdz-77@users.noreply.github.com>
2024-03-18 17:27:11 -0300

    fix type errors

--- sections/Product/BuyTogether.tsx
+++ sections/Product/BuyTogether.tsx
@@ -1,17 +1,21 @@
-import { ProductDetailsPage } from "apps/commerce/types.ts";
+import { Product, ProductDetailsPage } from "apps/commerce/types.ts";
 import { AppContext } from "deco-sites/bianza/apps/site.ts";
 import ProductCard from "deco-sites/bianza/components/product/ProductCard.tsx";
+import { SectionProps } from "deco/types.ts";
+import type { Manifest as ManifestVNDA } from "apps/vnda/manifest.gen.ts";
+import { ManifestOf } from "deco/mod.ts";
+import { InvocationProxy } from "deco/utils/invoke.types.ts";
 
 export interface Props {
   page: ProductDetailsPage | null;
 }
 
-export default function BuyTogether({ products }: ReturnType<typeof loader>) {
+export default function BuyTogether({ products }: SectionProps<typeof loader>) {
   return (
     <>
       <h1>Buy Together</h1>
       <div class="flex">
-        {products.map((product) => {
+        {products?.map((product) => {
           return <ProductCard product={product} />;
         })}
       </div>
@@ -19,17 +23,19 @@ export default function BuyTogether({ products }: ReturnType<typeof loader>) {
   );
 }
 
-export const loader = async (props: Props, req: Request, ctx: AppContext) => {
+type Vnda = InvocationProxy<ManifestVNDA>;
+
+export const loader = async (props: Props, _req: Request, ctx: AppContext) => {
   // Buscar os produtos do compre junto
 
   const tag = props.page?.product.additionalProperty?.find((add) =>
     add.name === "compre-junto" || add.value === "compre-junto"
   );
-  let products = [];
+  let products: Product[] | null = null;
   if (tag) {
-    products = await ctx.invoke.vnda.loaders.productList({
+    products = await (ctx.invoke as unknown as Vnda).vnda.loaders.productList({
       typeTags: [
-        { key: "compre-junto", value: tag.value },
+        { key: "compre-junto", value: tag.value || "" },
       ],
     });
   }
